73ea0906a7c8890daae1f3a8ce5bc3771c45bbfd
mysterious-man01 <joaop3dr03090@gmail.com>
2026-01-05 13:20:23 -0300

    Add type command

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 std::string echo(std::string str);
 
+std::string type(std::string str);
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -22,12 +24,19 @@ int main() {
     end_cmd_pos = prompt.find(' ', 0);
     command = prompt.substr(0, end_cmd_pos);
 
+    // Exits shell
     if(command == "exit"){
       return 0;
     }
     
+    // Call command echo
     else if(command == "echo"){
       std::cout << echo(prompt.substr(end_cmd_pos + 1, prompt.find('\n', 0))) << '\n';
+    }
+
+    // Call command type
+    else if(command == "type"){
+      std::cout << type(prompt.substr(end_cmd_pos + 1, prompt.find('\n', 0))) << '\n';
     }
 
     else{
@@ -41,4 +50,22 @@ int main() {
 // echo command
 std::string echo(std::string str){
   return str;
+}
+
+// type command
+std::string type(std::string str){
+  std::string cmd;
+  size_t end_cmd_pos;
+
+  end_cmd_pos = str.find(' ', 0);
+  if(end_cmd_pos != str.npos){
+    cmd = str.substr(0, end_cmd_pos);
+  }
+  else cmd = str;
+
+  if(cmd == "exit" || cmd == "echo" || cmd == "type"){
+    return cmd + " is a shell buildin";
+  }
+  
+  return cmd + ": not found";
 }
