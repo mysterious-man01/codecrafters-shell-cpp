d6a7c40a26a9a941c52c3015070f7b1d2f1951e9
mysterious-man01 <joaop3dr03090@gmail.com>
2026-01-05 12:54:12 -0300

    add echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 #include <string>
 
+std::string echo(std::string str);
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::string prompt;
+  std::string command;
 
-  // TODO: Uncomment the code below to pass the first stage
+  size_t end_cmd_pos;
+
+  // Shell REPL loop
   do{
     std::cout << "$ ";
 
     std::getline(std::cin, prompt);
-    if(prompt == "exit"){
+    
+    end_cmd_pos = prompt.find(' ', 0);
+    command = prompt.substr(0, end_cmd_pos);
+
+    if(command == "exit"){
       return 0;
     }
     
-    std::cout << prompt << ": command not found" << "\n";
+    else if(command == "echo"){
+      std::cout << echo(prompt.substr(end_cmd_pos + 1, prompt.find('\n', 0))) << '\n';
+    }
+
+    else{
+      std::cout << command << ": command not found" << "\n";
+    }
   } while(true);
 
   return 0;
+}
+
+// echo command
+std::string echo(std::string str){
+  return str;
 }
