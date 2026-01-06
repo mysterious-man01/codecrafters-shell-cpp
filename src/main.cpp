997027804111bc17072574068bab96f9966c0c7b
mysterious-man01 <joaop3dr03090@gmail.com>
2026-01-06 09:54:07 -0300

    Add check PATH variable

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 std::string echo(std::string str);
 
 std::string type(std::string str);
+
+const std::string PATH = std::getenv("PATH");
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -15,6 +20,8 @@ int main() {
 
   size_t end_cmd_pos;
 
+  std::cout << "PATH=" << PATH << std::endl; // Remove
+
   // Shell REPL loop
   do{
     std::cout << "$ ";
@@ -24,6 +31,8 @@ int main() {
     end_cmd_pos = prompt.find(' ', 0);
     command = prompt.substr(0, end_cmd_pos);
 
+    if(command.empty()) continue;
+
     // Exits shell
     if(command == "exit"){
       return 0;
@@ -31,12 +40,12 @@ int main() {
     
     // Call command echo
     else if(command == "echo"){
-      std::cout << echo(prompt.substr(end_cmd_pos + 1, prompt.find('\n', 0))) << '\n';
+      std::cout << echo(prompt.substr(end_cmd_pos + 1, (prompt.find('\n', 0) - end_cmd_pos))) << '\n';
     }
 
     // Call command type
     else if(command == "type"){
-      std::cout << type(prompt.substr(end_cmd_pos + 1, prompt.find('\n', 0))) << '\n';
+      std::cout << type(prompt.substr(end_cmd_pos + 1, (prompt.find('\n', 0) - end_cmd_pos))) << '\n';
     }
 
     else{
@@ -56,6 +65,7 @@ std::string echo(std::string str){
 std::string type(std::string str){
   std::string cmd;
   size_t end_cmd_pos;
+  
 
   end_cmd_pos = str.find(' ', 0);
   if(end_cmd_pos != str.npos){
@@ -63,8 +73,46 @@ std::string type(std::string str){
   }
   else cmd = str;
 
-  if(cmd == "exit" || cmd == "echo" || cmd == "type"){
+  if(cmd.empty()) return "";
+
+  if(cmd == "exit" || cmd == "echo" || cmd == "type")
     return cmd + " is a shell builtin";
+  
+  size_t offset = 0, temp; 
+  const size_t npos = PATH.npos;
+  std::string path_dir;
+  
+  while(offset != npos){
+    if(offset != 0) offset++;
+
+    temp = PATH.find(':', offset);
+    if(temp != npos){
+      path_dir = PATH.substr(offset, temp - offset);
+      offset = temp;
+    }
+    else{
+      path_dir = PATH;
+      offset = temp;
+    }
+
+    if(path_dir.empty()) continue;
+
+    if(fs::exists(path_dir)){
+      try{
+        for(const auto& entry : fs::directory_iterator(path_dir)){
+          if(cmd == entry.path().filename() && fs::is_regular_file(entry)){
+            fs::perms perms = fs::status(entry).permissions();
+
+            if((perms & fs::perms::owner_exec) != fs::perms::none || 
+              (perms & fs::perms::group_exec) != fs::perms::none || 
+              (perms & fs::perms::others_exec) != fs::perms::none)
+              return cmd + " is " + entry.path().string();
+          }
+        }
+      } catch(const fs::filesystem_error& e){
+        std::cerr << "Error accessing directory: " << e.what() << std::endl;
+      }
+    }
   }
   
   return cmd + ": not found";
